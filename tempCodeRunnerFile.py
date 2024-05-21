ac56da36b8b91f763e6fb612784b3b39c561e641
mein9 <mateo0221@gmail.com>
2024-05-21 08:49:50 -0700

    Fixed Game to show word on one line

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,7 @@
 import random
+import math
 
-name = input("What is your name?")
+name = input()
 
 print(f'Good Luck {name}!')
 
@@ -22,21 +23,21 @@ while turns > 0:
         if char in guesses:
             print(char, end=" ")
         else:
-            print("_", end=" ")
+            print("_")
             failed +=1
-    print()
+    
     if failed == 0:
         print("You Win")
         print("The word is: ", word)
         break
 
+print()
+guess = input("Guess a character:")
+guesses += guess
+if guess not in word:
+    turns -= 1
+    print("Wrong")
+    print(f'You have {turns} more guesses')
 
-    guess = input("Guess a character:")
-    guesses += guess
-    if guess not in word:
-        turns -= 1
-        print("Wrong")
-        print(f'You have {turns} more guesses')
-
-        if turns == 0:
-            print("You Lose")
+    if turns == 0:
+        print("You Lose")
